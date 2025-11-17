dc4e397a6fa382c964695f79e5f47eb0f1daf180
a0507112 <r-arya@ti.com>
2025-11-17 21:12:16 +0530

    Added folding for grug-far
    - lazy-lock updated as well with updates

---
+++ nvim/.config/nvim/lua/plugins/grug-far.lua
@@ -0,0 +1,10 @@
+return {
+    "MagicDuck/grug-far.nvim",
+    opts = {
+        folding = {
+            enabled = true,
+            -- Include the file path line in the fold so you can fold at the file level
+            include_file_path = true,
+        }
+    }
+}
